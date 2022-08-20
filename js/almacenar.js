b7cd79b4000207fdfacdb0102595bdad2d05825a
Nicolas Silva Augustyniak <nicoaugustyniak@gmail.com>
2022-08-20 19:34:01 -0300

    Funciones añadidas: Los items ahora son almacenados en el local storage, al recargar la pagina o cerrar el navegador los items vuelven a mostrarse como estaban, boton Limpiar borra todos los items de la lista y del local strorage.

--- js/almacenar.js
+++ js/almacenar.js
@@ -6,58 +6,120 @@ function showAlertError() {
   document.getElementById("alert-danger").classList.add("show");
 }
 
-// Pauta
+//* Pauta
 
-// 1 - Realicen las modificaciones necesarias para que al pulsar el botón Agregar, y si hay contenido en el campo para añadir nuevos ítems:
+//* 1 - Realicen las modificaciones necesarias para que al pulsar el botón Agregar, y si hay contenido en el campo para añadir nuevos ítems:
 
-// Se agregue el nuevo ítem al listado guardado
-// Se actualice la vista del listado
-// Se limpie el campo para añadir nuevos ítems
+//* Se agregue el nuevo ítem al listado guardado
+//* Se actualice la vista del listado
+//* Se limpie el campo para añadir nuevos ítems
 
-// 2 - El contenido del listado (y su visualización) deben mantenerse aún cuando el navegador se cierre y se vuelva a abrir.
+//* 2 - El contenido del listado (y su visualización) deben mantenerse aún cuando el navegador se cierre y se vuelva a abrir.
 
-// 3 - Realicen las modificaciones necesarias para que al pulsar el botón Limpiar:
+//* 3 - Realicen las modificaciones necesarias para que al pulsar el botón Limpiar:
 
-// Se elimine el listado almacenado
-// Se actualice la vista del listado (ahora vacío)
+//* Se elimine el listado almacenado
+//* Se actualice la vista del listado (ahora vacío)
 
-// Entidad del html donde se va a cargar la lista
-const contenedor = document.getElementById("contenedor");
+// * ---------- DOM ----------
 
-// Se inicia la lista vacia
-let itemList = [];
+const btn_agregar = document.getElementById("agregar");
 
-// La funcionalidad se implementa al hacer click en el boton agregar
-agregar.addEventListener("click", function () {
-  clearList();
+const btn_limpiar = document.getElementById("limpiar");
 
-  const input = document.getElementById("item");
+const item = document.getElementById("item");
 
-  if (input.value === "") {
+const cont_items = document.getElementById("contenedor");
+
+// * ---------- BOTONES ----------
+
+// AL CLICKEAR BOTON AGREGAR
+btn_agregar.onclick = () => {
+  if (item.value === "") {
     showAlertError();
     return;
   }
+  addItem(item.value);
+  item.value = "";
+};
 
-  itemList.push(input.value);
-  addToList();
+// AL CLICKEAR BOTON LIMPIAR
+btn_limpiar.onclick = () => {
+  localStorage.clear();
+  location.reload();
+};
 
-  input.value = "";
-});
+//* ---------- LOCAL STORAGE ----------
 
-// Funcion que hace un loop entre los items de la lista y los muetra en pantalla
-function addToList() {
-  if (itemList.length > 0) {
-    itemList.forEach((item) => {
-      contenedor.innerHTML += `<li class="list-group-item list-group-item-action
-      ">${item}</li>`;
-    });
+// Se inicia un cotador en 0 y una lista vacia
+let counter = 0;
+let items_list = [];
+
+// Setea el contador en el local storage
+const setCounter = () => {
+  localStorage.setItem("counter", counter);
+};
+
+// Se utiliza para inicializar el contador al cargar la pagina
+const initializeCounter = () => {
+  counter = getCounter();
+};
+
+// Setea la lista items_list en el local storage
+const setItemList = () => {
+  localStorage.setItem("items_list", JSON.stringify(items_list));
+  addToHTML();
+};
+
+// Metodo getter que obtiene los datos del contador local storage
+const getCounter = () => {
+  const cont = localStorage.getItem("counter");
+  return cont;
+};
+
+// Metodo getter que obtiene los datos de la lista del local storage
+const getItemList = () => {
+  // Se ejecuta setCounter() para amuentar el contador por item ingresado
+  setCounter();
+  const list = JSON.parse(localStorage.getItem("items_list"));
+  return list;
+};
+
+// Agrega items a la lista del local storage y aumenta el contador previamente declarado en 1
+const addItem = (description) => {
+  counter++;
+  let objItem = {
+    id: counter,
+    description: description,
+  };
+
+  if (getItemList() != null) {
+    items_list = getItemList();
   }
-}
 
-function clearList() {
-  contenedor.innerHTML = "";
-}
+  items_list.push(objItem);
+  setItemList();
+};
 
-// limpiar.addEventListener("click", function () {
-//   // limpiar lista y storage
-// });
+//Añade el item al contenedor del HTML
+const addToHTML = () => {
+  cont_items.innerHTML = "";
+
+  if (getItemList() != null) {
+    for (let item of getItemList()) {
+      cont_items.innerHTML += `<li id="${item.id}" class="list-group-item list-group-item-action
+  ">${item.description} </li> `;
+    }
+  }
+};
+
+// * ---------- INICIADORES ----------
+
+// Iniciadores necesarios para mostrar los datos guardados luego de recargar o cerar el navegador
+initializeCounter();
+addToHTML();
+
+// Algunas de las funciones no son necesarias de momento pero permitiran agregar mas funciones a la pagina en un futuro
+
+// Realizado por: Joaquín Arnold Leivas
+//                Nicolas Silva Augustyniak
